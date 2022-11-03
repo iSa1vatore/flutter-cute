658292443b7ac3be2ed433b99b8b01dca489faf0
Salvatore <Skaldak1337@gmail.com>
2022-11-03 08:01:27 +0500

    Fix ticker providers export

--- packages/cute_wm/CHANGELOG.md
+++ packages/cute_wm/CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.0.2
+
+* Fix ticker providers export;
+
 ## 0.0.1
 
 * Initial release;
--- packages/cute_wm/lib/cute_wm.dart
+++ packages/cute_wm/lib/cute_wm.dart
@@ -1,3 +1,4 @@
 library cute_wm;
 
 export 'package:cute_wm/src/core.dart' hide CuteElement;
+export 'package:cute_wm/src/ticker/ticker_providers.dart';
